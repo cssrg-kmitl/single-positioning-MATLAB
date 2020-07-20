3ecf451fe35836fe566dea83f948c7d4ded84c19
cssrg-kmitl <67050737+cssrg-kmitl@users.noreply.github.com>
2020-07-20 14:28:08 +0700

    Update ProcessPositioning.m

--- ProcessPositioning.m
+++ ProcessPositioning.m
@@ -1,4 +1,4 @@
-%% Positioning calculation from RINEX 2.11
+% Positioning calculation from RINEX 2.11
 % Calculate user position by using the single point method
 % Original by Napat Tongkasem
 % Version 1.00 
@@ -30,7 +30,7 @@ warning off
 tic
 % 1. copy RINEX v 2.11 to /RINEX folder
 % 2. define RINEX file's name
-%% RINEX file
+% RINEX file
 r_o_name = 'KMIT2000.20o'; % observation file's name
 r_n_name = 'KMIT2000.20n'; % navigation file's name (if blank, program will downloaded from IGS)
 % r_n_name = ''; % navigation file's name
@@ -42,7 +42,7 @@ R_path = [p_path 'RINEX\'];     % RINEX path
 S_path = [p_path 'Results\'];    % Results path
 path(path,[p_path 'function']);
 
-%% 1. Read RINEX (using readrinex .mex file)
+% 1. Read RINEX (using readrinex .mex file)
 % Check file
 checkfileRN(r_o_name,R_path);
 % Read RINEX  
@@ -51,11 +51,11 @@ year  = num2str(obs.date(1));
 month = num2str(obs.date(2),'%.2d');
 date  = num2str(obs.date(3),'%.2d');
 
-%% 2. Estimate user positioning
+% 2. Estimate user positioning
 positioning(obs,nav,doy,S_path);
 
 
-%% 3. Plot Error
+% 3. Plot Error
 ploterror(year,month,date,obs.station,S_path)
 toc
 % remove file (reset)
